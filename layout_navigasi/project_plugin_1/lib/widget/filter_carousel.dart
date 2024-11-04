6dbbff5658fee79a697e449884ac03be909fb57b
eko2004 <eko145758@gmail.com>
2024-11-04 16:00:06 +0700

    commit

---
+++ layout_navigasi/project_plugin_1/lib/widget/filter_carousel.dart
@@ -0,0 +1,167 @@
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+@immutable
+class PhotoFilterCarousel extends StatefulWidget {
+  const PhotoFilterCarousel({super.key});
+
+  @override
+  State<PhotoFilterCarousel> createState() => _PhotoFilterCarouselState();
+}
+
+class _PhotoFilterCarouselState extends State<PhotoFilterCarousel> {
+  final _filters = [
+    Colors.white,
+    ...List.generate(
+      Colors.primaries.length,
+      (index) => Colors.primaries[(index * 4) % Colors.primaries.length],
+    )
+  ];
+  final _filterColor = ValueNotifier<Color>(Colors.white);
+
+  void _onFilterChanged(Color value) {
+    _filterColor.value = value;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.black,
+      child: Stack(
+        children: [
+          Positioned.fill(child: _buildPhotoWithFilter()),
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+            child: _buildFilterSelector(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPhotoWithFilter() {
+    return ValueListenableBuilder(
+      valueListenable: _filterColor,
+      builder: (context, color, child) {
+        return Image.network(
+          'https://docs.flutter.dev/cookbook/img-files/effects/instagram-buttons/millennial-dude.jpg',
+          color: color.withOpacity(0.5),
+          colorBlendMode: BlendMode.color,
+          fit: BoxFit.cover,
+        );
+      },
+    );
+  }
+
+  Widget _buildFilterSelector() {
+    return FilterSelector(
+      onFilterChanged: _onFilterChanged,
+      filters: _filters,
+    );
+  }
+}
+
+class FilterSelector extends StatelessWidget {
+  const FilterSelector({
+    Key? key,
+    required this.filters,
+    required this.onFilterChanged,
+  }) : super(key: key);
+
+  final List<Color> filters;
+  final void Function(Color selectedColor) onFilterChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: Flow(
+        delegate: CarouselFlowDelegate(
+          viewportOffset: ViewportOffset.zero(),
+          filtersPerScreen: 5,
+        ),
+        children: [
+          for (var color in filters)
+            FilterItem(
+              color: color,
+              onFilterSelected: () => onFilterChanged(color),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class FilterItem extends StatelessWidget {
+  const FilterItem({
+    Key? key,
+    required this.color,
+    required this.onFilterSelected,
+  }) : super(key: key);
+
+  final Color color;
+  final VoidCallback onFilterSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onFilterSelected,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: color,
+        ),
+        width: 60,
+        height: 60,
+      ),
+    );
+  }
+}
+
+class CarouselFlowDelegate extends FlowDelegate {
+  CarouselFlowDelegate({
+    required this.viewportOffset,
+    required this.filtersPerScreen,
+  }) : super(repaint: viewportOffset);
+
+  final ViewportOffset viewportOffset;
+  final int filtersPerScreen;
+
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    final count = context.childCount;
+    final size = context.size.width;
+    final itemExtent = size / filtersPerScreen;
+    final active = viewportOffset.pixels / itemExtent;
+    final min = math.max(0, active.floor() - 3).toInt();
+    final max = math.min(count - 1, active.ceil() + 3).toInt();
+
+    for (var index = min; index <= max; index++) {
+      final itemXFromCenter = itemExtent * index - viewportOffset.pixels;
+      final percentFromCenter = 1.0 - (itemXFromCenter / (size / 2)).abs();
+      final itemScale = 0.5 + (percentFromCenter * 0.5);
+      final opacity = 0.25 + (percentFromCenter * 0.75);
+      final itemTransform = Matrix4.identity()
+        ..translate((size - itemExtent) / 2)
+        ..translate(itemXFromCenter)
+        ..translate(itemExtent / 2, itemExtent / 2)
+        ..multiply(Matrix4.diagonal3Values(itemScale, itemScale, 1.0))
+        ..translate(-itemExtent / 2, -itemExtent / 2);
+
+      context.paintChild(
+        index,
+        transform: itemTransform,
+        opacity: opacity,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CarouselFlowDelegate oldDelegate) {
+    return oldDelegate.viewportOffset != viewportOffset;
+  }
+}
